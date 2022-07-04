c7063f2be25b8ff987dbc737eb1e10d8e8bed060
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-07-04 17:46:24 +1200

    Add rendering locations in locations component

--- client/components/Locations.jsx
+++ client/components/Locations.jsx
@@ -1,23 +1,48 @@
 import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { fetchRegions} from '../actions'
+import { fetchRegions, fetchLocations } from '../actions'
+//#
+import Regions from './Regions'
 
-function Regions () {
-  const regions = useSelector(state => state.regions)
+function Locations () {
+  const locations = useSelector(state => state.locations)
   const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(fetchRegions())
+    dispatch(fetchLocations())
   }, [])
 
+
+
+  // if region_id (locations table) matches id (regions table) display
+  //useParams
+    // const params = useParams()
+    // const id = fetchLocations
+
+    // const location = locations.find((location) => {
+    //   return location.id === Regions.id
+    // }) 
+    // if(location == null) {
+    //   return <h2>Not Found</h2>
+    // }
   return (
     <>
-      <div className="regions-container">
+      <div className="locations-container">
        <h1>Hello</h1>
+       <ul>
+          {locations?.map(location => (
+            <li key={location.id}>
+              {location.name}
+              <p>{location.description}</p>
+              <p>{location.address}</p>
+              <p>{location.opening_hours}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   )
 }
 
-export default Regions
+export default Locations
