8d0997d72cd5551b3df9146306cbe0ec19e4b812
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-07-05 15:50:33 +1200

    Add location form component - partial

--- client/components/AddLocationForm.jsx
+++ client/components/AddLocationForm.jsx
@@ -1,26 +1,108 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
 
-import { addLocation } from '../api'
-
-function AddLocation({ setLocations, closeAddForm, setError }) {
-  const locations = useSelector((state) => state.locations)
-  const [newLocation, setNewLocation] = useState()
+function AddLocation(props) {
+  const [newLocation, setNewLocation] = useState({
+    name: '',
+    description: '',
+    address: '',
+    opening_hours: '',
+    website_url: '',
+    wheelchair_compatible: '',
+    ramps: '',
+    elevator: '',
+    accessible_toilets: '',
+    region_id: '',
+  })
   //where to list all fields
-  function handleAddChange(e) {
-    //dispatch?
-    const { name, value } = e.target
-    setNewLocation({
-      ...newLocation,
-      [name]: value,
-    })
+
+  function handleSubmit(evt) {
+    evt.preventDefault()
+    props.setLocations([
+      ...props.locations,
+      {
+        ...newLocation,
+      },
+    ])
   }
 
-  function handleAdd() {
-    const location = { ...newLocation }
-    addLocation(location)
-      .then(setLocations)
-      .then(closeAddForm)
-      .catch((err) => setError(err.message))
+  function handleChange(evt) {
+    setNewLocation(evt.target.value)
   }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="locationName">name</label>
+      <input
+        type="text"
+        name="name"
+        value={newLocation.name}
+        onChange={handleChange}
+      />
+      <label htmlFor="locationDescription">description</label>
+      <input
+        type="text"
+        name="description"
+        value={newLocation.description}
+        onChange={handleChange}
+      />
+      <label htmlFor="locationAddress">address</label>
+      <input
+        type="text"
+        name="address"
+        value={newLocation.address}
+        onChange={handleChange}
+      />
+      <label htmlFor="locationHours">opening hours</label>
+      <input
+        type="text"
+        name="openingHours"
+        value={newLocation.opening_hours}
+        onChange={handleChange}
+      />
+      <label htmlFor="locationUrl">website</label>
+      <input
+        type="text"
+        name="url"
+        value={newLocation.website_url}
+        onChange={handleChange}
+      />
+      <label htmlFor="locationWheelchair">wheelchair compatibility</label>
+      <input
+        type="number"
+        name="wheelchair access"
+        value={newLocation.wheelchair_compatible}
+        onChange={handleChange}
+      />
+      <label htmlFor="locationRamps">ramps</label>
+      <input
+        type="number"
+        name="ramps"
+        value={newLocation.ramps}
+        onChange={handleChange}
+      />
+      <label htmlFor="locationElevator">elevator</label>
+      <input
+        type="number"
+        name="elevator"
+        value={newLocation.elevator}
+        onChange={handleChange}
+      />
+      <label htmlFor="locationToilets">toilets</label>
+      <input
+        type="number"
+        name="accessible toilets"
+        value={newLocation.accessible_toilets}
+        onChange={handleChange}
+      />
+      <label htmlFor="locationRegionId">region id</label>
+      <input
+        type="number"
+        name="region id"
+        value={newLocation.region_id}
+        onChange={handleChange}
+      />
+    </form>
+  )
 }
+
+export default AddLocation
